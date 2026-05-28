66a2ef6267730538cd84813cf4d03cc0a63f4eab
mondragonyos <mondragonyoseth@gmail.com>
2026-05-28 14:08:50 -0600

    Configuración de Java 17 y dependencias listas

---
+++ ecopredict/backend/src/main/java/com/ecopredict/EcopredictApplication.java
@@ -0,0 +1,12 @@
+package com.ecopredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopredictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcopredictApplication.class, args);
+    }
+}
